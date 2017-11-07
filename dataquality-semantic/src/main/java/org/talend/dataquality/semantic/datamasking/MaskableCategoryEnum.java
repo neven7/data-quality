979b578e704e0fbc06fac1ccd1453425493302b5
msjian <msjian@talend.com>
2017-11-07 18:25:42 +0800

    TDQ-14147: improve the code

--- dataquality-semantic/src/main/java/org/talend/dataquality/semantic/datamasking/MaskableCategoryEnum.java
+++ dataquality-semantic/src/main/java/org/talend/dataquality/semantic/datamasking/MaskableCategoryEnum.java
@@ -20,42 +20,36 @@ import org.talend.dataquality.datamasking.FunctionType;
  */
 public enum MaskableCategoryEnum {
 
-    ADDRESS_LINE("Address Line", FunctionType.MASK_ADDRESS),
-    CITY("City", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    // COMPANY("Company", FunctionType.GENERATE_FROM_FILE_STRING_PROVIDED, "company.txt"), //$NON-NLS-2$
-    EMAIL("Email", FunctionType.MASK_EMAIL),
-    // FIRST_NAME("First Name", FunctionType.GENERATE_FROM_FILE_STRING_PROVIDED, "firstName.txt"), //$NON-NLS-1$//$NON-NLS-2$
-    // LAST_NAME("Last Name", FunctionType.GENERATE_FROM_FILE_STRING_PROVIDED, "lastName.txt"), //$NON-NLS-1$//$NON-NLS-2$
-    // FR_COMMUNE("FR Commune", FunctionType.GENERATE_FROM_FILE_STRING_PROVIDED, "commune.txt"), //$NON-NLS-2$
-    FULL_NAME("Full Name", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    IPv4_ADDRESS("IPv4 Address", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    IPv6_ADDRESS("IPv6 Address", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    // JOB_TITLE("Job Title", FunctionType.GENERATE_FROM_FILE_STRING_PROVIDED, "jobTitle.txt"), //$NON-NLS-2$
-    GEO_COORDINATE("Geographic coordinate", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    GEO_COORDINATES_DEG("Geographic coordinates (degrees)", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    MAC_ADDRESS("MAC Address", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    // ORGANIZATION("Organization", FunctionType.GENERATE_FROM_FILE_STRING_PROVIDED, "organization.txt"), //$NON-NLS-2$
-    PASSPORT("Passport", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
+    ADDRESS_LINE("Address Line", FunctionType.MASK_ADDRESS), //$NON-NLS-1$
+    CITY("City", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    EMAIL("Email", FunctionType.MASK_EMAIL), //$NON-NLS-1$
+    FULL_NAME("Full Name", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    IPv4_ADDRESS("IPv4 Address", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    IPv6_ADDRESS("IPv6 Address", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    GEO_COORDINATE("Geographic coordinate", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    GEO_COORDINATES_DEG("Geographic coordinates (degrees)", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    MAC_ADDRESS("MAC Address", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    PASSPORT("Passport", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
 
-    US_PHONE("US Phone", FunctionType.GENERATE_UNIQUE_PHONE_NUMBER_US),
-    FR_PHONE("FR Phone", FunctionType.GENERATE_UNIQUE_PHONE_NUMBER_FRENCH),
-    UK_PHONE("UK Phone", FunctionType.GENERATE_UNIQUE_PHONE_NUMBER_UK),
-    DE_PHONE("DE Phone", FunctionType.GENERATE_UNIQUE_PHONE_NUMBER_GERMANY),
+    US_PHONE("US Phone", FunctionType.GENERATE_UNIQUE_PHONE_NUMBER_US), //$NON-NLS-1$
+    FR_PHONE("FR Phone", FunctionType.GENERATE_UNIQUE_PHONE_NUMBER_FRENCH), //$NON-NLS-1$
+    UK_PHONE("UK Phone", FunctionType.GENERATE_UNIQUE_PHONE_NUMBER_UK), //$NON-NLS-1$
+    DE_PHONE("DE Phone", FunctionType.GENERATE_UNIQUE_PHONE_NUMBER_GERMANY), //$NON-NLS-1$
 
-    US_POSTAL_CODE("US Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    FR_POSTAL_CODE("FR Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    DE_POSTAL_CODE("DE Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    UK_POSTAL_CODE("UK Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    BE_POSTAL_CODE("BE Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
-    FR_CODE_COMMUNE_INSEE("FR Insee Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION),
+    US_POSTAL_CODE("US Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    FR_POSTAL_CODE("FR Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    DE_POSTAL_CODE("DE Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    UK_POSTAL_CODE("UK Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    BE_POSTAL_CODE("BE Postal Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
+    FR_CODE_COMMUNE_INSEE("FR Insee Code", FunctionType.REPLACE_CHARACTERS_WITH_GENERATION), //$NON-NLS-1$
 
-    US_SSN("US Social Security Number", FunctionType.GENERATE_SSN_US),
-    FR_SSN("FR Social Security Number", FunctionType.GENERATE_SSN_FRENCH),
-    UK_SSN("UK Social Security Number", FunctionType.GENERATE_SSN_UK),
+    US_SSN("US Social Security Number", FunctionType.GENERATE_SSN_US), //$NON-NLS-1$
+    FR_SSN("FR Social Security Number", FunctionType.GENERATE_SSN_FRENCH), //$NON-NLS-1$
+    UK_SSN("UK Social Security Number", FunctionType.GENERATE_SSN_UK), //$NON-NLS-1$
 
-    MASTERCARD("Mastercard Credit Card", FunctionType.GENERATE_CREDIT_CARD_FORMAT_STRING),
-    US_CREDIT_CARD("AmEx Credit Card", FunctionType.GENERATE_CREDIT_CARD_FORMAT_STRING),
-    VISACARD("Visa Credit Card", FunctionType.GENERATE_CREDIT_CARD_FORMAT_STRING);
+    MASTERCARD("Mastercard Credit Card", FunctionType.GENERATE_CREDIT_CARD_FORMAT_STRING), //$NON-NLS-1$
+    US_CREDIT_CARD("AmEx Credit Card", FunctionType.GENERATE_CREDIT_CARD_FORMAT_STRING), //$NON-NLS-1$
+    VISACARD("Visa Credit Card", FunctionType.GENERATE_CREDIT_CARD_FORMAT_STRING); //$NON-NLS-1$
 
     private String displayName;
 
