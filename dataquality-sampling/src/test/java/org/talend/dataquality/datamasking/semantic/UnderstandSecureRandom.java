c307858fe6f00c640dde61ca1bad26de40ba5a6c
zshen-talend <zshen@talend.com>
2018-05-31 15:20:18 +0800

    make linux use same seed with windows

--- dataquality-sampling/src/test/java/org/talend/dataquality/datamasking/semantic/UnderstandSecureRandom.java
+++ dataquality-sampling/src/test/java/org/talend/dataquality/datamasking/semantic/UnderstandSecureRandom.java
@@ -1,13 +1,14 @@
 package org.talend.dataquality.datamasking.semantic;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class UnderstandSecureRandom {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws NoSuchAlgorithmException {
 
-        for (int i=0;i<5;i++) {
-            SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < 5; i++) {
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG"); //$NON-NLS-1$
             secureRandom.setSeed(100L);
             System.out.println(secureRandom.nextInt());
         }
